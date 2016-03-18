ccca31d63960a8a3f54798cbe4cc8ee8431d03fe
Tatsushi Demachi <tdemachi@gmail.com>
2016-03-19 00:22:38 +0900

    Add and fix Puma unix domain socket path
    nginx redirects a site access to the Rails application via an unix
    domain socket, not a TCP connection for performance but Puma default
    configuration doesn't have such a listener. This adds it.
    
    Usually, an application specific unix domains socket file is placed in
    an application's tmp/sockets directory but VirtualBox vboxsf doesn't
    allow to make an unix domain socket file in its filesystem. So, instead
    of there, it put the application's unix domain socket file into
    /tmp/sockets.

--- bitkidsserver/config/puma.rb
+++ bitkidsserver/config/puma.rb
@@ -11,6 +11,8 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
+bind        "unix:///tmp/sockets/bitkidsserver-puma.sock"
+
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
